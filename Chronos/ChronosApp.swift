fed3b2775a42f59b7bca12ff46e188db2a02cfb0
SamarAssi <79410649+SamarAssi@users.noreply.github.com>
2024-04-05 17:30:50 +0200

    Initial Commit

---
+++ Chronos/ChronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronosApp.swift
+//  Chronos
+//
+//  Created by Samar Assi on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
