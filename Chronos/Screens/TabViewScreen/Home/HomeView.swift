d213e523e0b8b2dbd3c0401bcb30288bb76643ce
SamarAssi <79410649+SamarAssi@users.noreply.github.com>
2024-04-22 15:48:41 +0300

    delete testing code

--- Chronos/Screens/TabViewScreen/Home/HomeView.swift
+++ Chronos/Screens/TabViewScreen/Home/HomeView.swift
@@ -145,16 +145,6 @@ extension HomeView {
     }
 }
 
-struct ScrollOffsetPreferenceKey: PreferenceKey {
-    typealias Value = CGFloat
-    
-    static var defaultValue: CGFloat = 0.0
-    
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
-
 #Preview {
     HomeView()
 }
