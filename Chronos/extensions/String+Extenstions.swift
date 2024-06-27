9bddaf9ae3dc9487c0f12366aa10a9148975ae30
Tareq Khalili <tkhalili@restaurant365.com>
2024-06-27 13:04:28 +0300

    Fix date

--- Chronos/extensions/String+Extenstions.swift
+++ Chronos/extensions/String+Extenstions.swift
@@ -12,7 +12,6 @@ extension String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         return dateFormatter.date(from: self)
     }
 
